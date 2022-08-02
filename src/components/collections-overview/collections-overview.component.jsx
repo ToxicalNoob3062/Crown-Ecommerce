60b3478920aa469011262a708dd2f2cecdc53c3f
ToxicalNoob@3062 <rahat3062@gmail.com>
2022-08-02 15:55:17 +0600

    moved the js page data into reducer state and finally converted all  class components to redux and inroduced the one way data flow for whole site and also maintained and build some selectors to improve normal performance

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import {createStructuredSelector} from "reselect"
-import { selectShopItems } from "../../redux/shop/shop.selector.js";
-import './collections-overview.styles.scss'
-import CollectionPreview from '../preview-collection/preview-collection.component';
-const  CollectionsOverview=({collections})=>{
-return <div className='collection-overview'>
-  {collections.map(({ id, ...others }) => {
-    return <CollectionPreview key={id} {...others} />;
-  })}
-  </div>
-}
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector.js";
+import "./collections-overview.styles.scss";
+import CollectionPreview from "../preview-collection/preview-collection.component";
+const CollectionsOverview = ({ collections }) => {
+  return (
+    <div className="collection-overview">
+      {collections.map(({ id, ...others }) => {
+        return <CollectionPreview key={id} {...others} />;
+      })}
+    </div>
+  );
+};
 
-const mapStateToProps=createStructuredSelector({
-  collections:selectShopItems
-})
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview,
+});
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,6 +1,14 @@
 import { createSelector } from "reselect";
 
-export const selectShop=state=>state.shop
-export const selectShopItems=createSelector([selectShop],shop=>{
-  return shop.collections
-})
+export const selectShop = (state) => state.shop;
+export const selectCollections = createSelector([selectShop], (shop) => {
+  return shop.collections;
+});
+export const selectCollectionsForPreview = createSelector([selectCollections], (collections) => {
+  return Object.values(collections);
+});
+export const selectCollection = (collectionUrlParams) => {
+  return createSelector([selectCollections], (collections) => {
+    return collections[collectionUrlParams];
+  });
+};
